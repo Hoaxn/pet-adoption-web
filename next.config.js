7109dd361bf080e0625360c65818ad0a5ae19490
Hoaxn <rockemu77@gmail.com>
2023-09-09 22:07:51 +0545

    Some changes

--- next.config.js
+++ next.config.js
@@ -27,83 +27,11 @@ const nextConfig = {
         destination: "/register",
         permanent: true,
       },
-      // {
-      //   source: "/termsofservice",
-      //   destination: "/terms-of-service",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/privacypolicy",
-      //   destination: "/privacypolicy",
-      //   permanent: true,
-      // },
       {
         source: "/register/startnow",
         destination: "/register",
         permanent: true,
       },
-      // {
-      //   source:
-      //     "/pos-system-how-can-the-right-one-help-improve-a-small-business",
-      //   destination:
-      //     "/blogs/pos-system-how-can-the-right-one-help-improve-a-small-business",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/lucapary",
-      //   destination: "/lucapay",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/trace",
-      //   destination: "https://posapt.au",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/enquire",
-      //   destination: "/enquiry",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/storelist.html",
-      //   destination: "https://posapt.au",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/build-manifest.json",
-      //   destination: "https://posapt.au",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/package.json",
-      //   destination: "https://posapt.au",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/react-loadable-manifest.json",
-      //   destination: "https://posapt.au",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/server.js",
-      //   destination: "https://posapt.au",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/home",
-      //   destination: "https://posapt.au",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/register/startnow",
-      //   destination: "/register",
-      //   permanent: true,
-      // },
-      // {
-      //   source: "/partners",
-      //   destination: "https://posapt.au",
-      //   permanent: true,
-      // },
     ];
   },
 };
